b6004582309f2c72f670da9c3bae0c1eed491ee6
Rahim10020 <rahialighi@gmail.com>
2026-03-29 22:08:12 +0000

    add some icons

---
+++ src/shared/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from "react";
+
+type IconProps = SVGProps<SVGSVGElement>;
+
+export function ArrowLeftIcon(props: IconProps) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M3 12L8 17M3 12L8 7M3 12H21"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/shared/icons/ArrowRightIcon.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from "react";
+
+type IconProps = SVGProps<SVGSVGElement>;
+
+export function ArrowRightIcon(props: IconProps) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M21 12L16 7M21 12L16 17M21 12H3"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
