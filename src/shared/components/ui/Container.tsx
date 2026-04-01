1c3fb3587fca9b52e6a771675a7b689bf587f6e9
Rahim10020 <rahialighi@gmail.com>
2026-04-01 12:24:59 +0000

    modif

--- src/shared/components/ui/Container.tsx
+++ src/shared/components/ui/Container.tsx
@@ -10,7 +10,7 @@ export default function Container(props: ContainerProps) {
   const { children, className } = props;
   return (
     <div
-      className={twMerge("mx-auto max-w-6xl px-4 sm:px-6 lg:px-0", className)}
+      className={twMerge("mx-auto max-w-6xl px-4 sm:px-6 lg:px-2", className)}
     >
       {children}
     </div>
