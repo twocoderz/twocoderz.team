3e7e04bf373e6b9f051c917505af0a5e544f4fa0
Rahim10020 <rahialighi@gmail.com>
2026-04-03 14:59:41 +0000

    finish adjustments

--- src/shared/components/ui/NavItem.tsx
+++ src/shared/components/ui/NavItem.tsx
@@ -14,13 +14,13 @@ export default function NavItem(props: NavItemProps) {
     <a
       href={href}
       className={twMerge(
-        "group relative inline-block pb-px text-black-80 text-sm transition-colors duration-300 ease-out hover:text-black",
+        "group relative inline-block pb-p1 text-black-70 text-sm transition-colors duration-300 ease-out hover:text-black-90",
         className,
       )}
     >
       {children}
       <span
-        className="pointer-events-none absolute bottom-0 left-0 right-0 h-0.5 origin-left scale-x-0 bg-black transition-transform duration-300 ease-out group-hover:scale-x-100"
+        className="pointer-events-none absolute bottom-0 left-0 right-0 h-0.5 origin-left scale-x-0 bg-black-60 transition-transform duration-300 ease-out group-hover:scale-x-100"
         aria-hidden
       />
     </a>
