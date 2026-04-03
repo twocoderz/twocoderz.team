37fc43f54b498eed29bd49be44ac44550929a967
Rahim10020 <rahialighi@gmail.com>
2026-04-03 13:43:09 +0000

    modif

--- src/shared/components/ui/MyUser.tsx
+++ src/shared/components/ui/MyUser.tsx
@@ -9,9 +9,9 @@ export default function MyUser(props: MyUserProps) {
   return (
     <div className="flex items-center gap-p6">
       <img src={src} alt={`${name} image`} className="w-12 h-12 rounded-full" />
-      <div className="flex flex-col items-start gap-p4">
+      <div className="flex flex-col items-start gap-p2">
         <h4 className="text-black-80 font-bold text-lg">{name}</h4>
-        <p className="text-black-50 font-normal text-base">{profession}</p>
+        <p className="text-black-50 font-normal text-sm">{profession}</p>
       </div>
     </div>
   );
