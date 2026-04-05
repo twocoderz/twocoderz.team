dc75e2da9f02b406ecd28dd3ea64a89abbe479b5
Rahim10020 <rahialighi@gmail.com>
2026-04-05 05:00:52 +0000

    evolution

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -2,6 +2,7 @@ import Layout from "../../shared/components/layout/Layout";
 import { HeroSection } from "../../shared/components/sections/HeroSection";
 import Container from "../../shared/components/ui/Container";
 import Section from "../../shared/components/ui/Section";
+import EmailLink from "./components/EmailLink";
 
 export default function ContactPage() {
   return (
@@ -14,12 +15,15 @@ export default function ContactPage() {
 
       <Section>
         <Container>
-          <div className="max-w-2xl mx-auto">
-            {/* TODO: Add contact form */}
-            {/* TODO: Add contact info */}
-            {/* TODO: Add social links */}
-            {/* TODO: Add map or location info */}
-            <div className="text-center"></div>
+          <div className="max-w-2xl mx-auto flex flex-col items-center">
+            {/* email link */}
+            <EmailLink href="#" label="rahim100codeur@gmail.com" />
+            {/* separator */}
+            <div className="my-6 flex w-full items-center gap-4">
+              <div className="h-px flex-1 bg-black/20" />
+              <span className="text-sm text-black/60">Or</span>
+              <div className="h-px flex-1 bg-black/20" />
+            </div>
           </div>
         </Container>
       </Section>
--- src/pages/contact/components/EmailLink.tsx
+++ src/pages/contact/components/EmailLink.tsx
@@ -12,9 +12,12 @@ export default function EmailLink(props: EmailLinkProps) {
   return (
     <a
       href={href}
-      className={twMerge("cursor-pointer hover:underline", className)}
+      className={twMerge(
+        "cursor-pointer hover:underline flex items-center gap-p2",
+        className,
+      )}
     >
-      <span className="text-xl font-bold text-black-80">{label}</span>
+      <span className="text-2xl font-bold text-black-80">{label}</span>
       <CopyIcon className="text-black-80" />
     </a>
   );
