42e26f4ed70790a4206889c04f32da9af238d147
Rahim10020 <rahialighi@gmail.com>
2026-07-30 17:57:15 +0000

    remove container in projects page

--- src/pages/home/components/Projects.tsx
+++ src/pages/home/components/Projects.tsx
@@ -1,4 +1,3 @@
-import Container from "../../../shared/components/ui/Container";
 import { ProjectCard } from "../../../shared/components/ui/ProjectCard";
 import { portfolioData } from "../../../shared/data/portfolio";
 
@@ -13,29 +12,27 @@ export default function Projects() {
 
   return (
     <section>
-      <Container>
-        {/* Mobile: une seule colonne */}
-        <div className="space-y-24 md:hidden mt-p18">
-          {displayedProjects.map((project) => (
+      {/* Mobile: une seule colonne */}
+      <div className="space-y-24 md:hidden mt-p18">
+        {displayedProjects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
+
+      {/* Desktop: deux colonnes flex sans CSS columns */}
+      <div className="hidden md:flex md:items-start md:gap-8 mt-p24">
+        <div className="flex-1 space-y-24">
+          {leftColumnProjects.map((project) => (
             <ProjectCard key={project.name} project={project} />
           ))}
         </div>
 
-        {/* Desktop: deux colonnes flex sans CSS columns */}
-        <div className="hidden md:flex md:items-start md:gap-8 mt-p24">
-          <div className="flex-1 space-y-24">
-            {leftColumnProjects.map((project) => (
-              <ProjectCard key={project.name} project={project} />
-            ))}
-          </div>
-
-          <div className="flex-1 space-y-24">
-            {rightColumnProjects.map((project) => (
-              <ProjectCard key={project.name} project={project} />
-            ))}
-          </div>
+        <div className="flex-1 space-y-24">
+          {rightColumnProjects.map((project) => (
+            <ProjectCard key={project.name} project={project} />
+          ))}
         </div>
-      </Container>
+      </div>
     </section>
   );
 }
