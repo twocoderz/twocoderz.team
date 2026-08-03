741455661b587446581d1271bc2a39435ed58243
Rahim10020 <rahialighi@gmail.com>
2026-08-03 16:36:51 +0000

    evolution

--- src/pages/work/WorkDetailPage.tsx
+++ src/pages/work/WorkDetailPage.tsx
@@ -20,14 +20,56 @@ export default function WorkDetailPage() {
           <div>
             {/* Top */}
             <div className="flex flex-col items-start gap-8">
-              <h1 className="text-4xl lg:text-6xl font-medium">Clearbit</h1>
+              <h1 className="text-4xl lg:text-8xl font-medium">Clearbit</h1>
               <img
                 src="https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800"
                 alt="image"
                 className="w-full h-150 rounded-lg objec-cover"
               />
-            </div>git 
+            </div>
             {/* details */}
+            <div className="">
+              <div className="flex flex-col gap-8">
+                <h3>About</h3>
+                <p>
+                  Clearbit, a business intelligence platform, engaged us to
+                  enhance its digital presence. We revamped their website to
+                  better reflect their brand evolution, improving visuals and
+                  user experience. This redesign resulted in clearer messaging,
+                  a stronger brand image and increased conversion rates.
+                </p>
+              </div>
+              <div className="flex flex-col lg:flex-row gap-4">
+                <div>
+                  <div>
+                    <div>
+                      <span>Exit</span>
+                      <h4>$150M</h4>
+                    </div>
+                    <p>Acquisition by Hubspot</p>
+                  </div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>
+                    <span>B2B data</span>
+                    <h4>50M</h4>
+                  </div>
+                  <p>Company records</p>
+                </div>
+                <div>
+                  <div>
+                    <span>Usage</span>
+                    <h4>1500</h4>
+                  </div>
+                  <p>Business customers</p>
+                </div>
+              </div>
+            </div>
+            {/* rest */}
             <div></div>
           </div>
         </Container>
