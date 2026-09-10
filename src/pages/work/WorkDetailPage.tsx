ebfb5c77a6ee4a33b968323946906563de475115
Rahim10020 <rahialighi@gmail.com>
2026-09-10 17:45:02 +0000

    coding work detail

--- src/pages/work/WorkDetailPage.tsx
+++ src/pages/work/WorkDetailPage.tsx
@@ -105,7 +105,68 @@ export default function WorkDetailPage() {
               </div>
             </div>
             {/* rest */}
-            <div></div>
+            <div className="my-12">
+              <div className="mx-auto max-w-3xl flex flex-col gap-4 ">
+                <p className="text-2xl font-normal leading-relaxed text-black-90">
+                  Rizzle began when visionary founders with a small group of
+                  developers approached us with a clear mission: to create the
+                  ultimate discussion platform for the Instagram generation.
+                  Initially, the app targeted a specific audience, with its
+                  concept and core features meticulously structured and defined.
+                </p>
+                <p className="text-2xl font-normal leading-relaxed text-black-90">
+                  Since our partnership began in late 2018, Rizzle has undergone
+                  significant transformations to become what it is today. One of
+                  the notable iterations was a social network for creators
+                  available on both iOS and Android.
+                </p>
+                <p className="text-2xl font-normal leading-relaxed text-black-90">
+                  At its peak as a social network, Rizzle boasted over 34
+                  million monthly active users. Today, leveraging cutting-edge
+                  artificial intelligence (AI), Rizzle has evolved into a
+                  revolutionary "no-edit" video creation platform dedicated to
+                  democratizing video content creation.
+                </p>
+              </div>
+            </div>
+            <div className="my-12">
+              <img
+                src="https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800"
+                alt="image"
+                className="w-full h-150 rounded-lg objec-cover"
+              />
+            </div>
+            <div>
+              <div className="mx-auto max-w-3xl flex flex-col gap-4">
+                <h3 className="text-6xl text-black-90 font-bold">Challenge</h3>
+                <p className="text-2xl font-normal leading-relaxed text-black-90">
+                  The carbon management market is full of technically complex,
+                  enterprise-focused tools like Watershed and Persefoni. These
+                  platforms often require lengthy onboarding, deliver slow
+                  pricing quotes, and can be inaccessible for companies without
+                  dedicated sustainability teams. Upstream&apos;s challenge was
+                  to:
+                </p>
+                <ul className="flex flex-col gap-4">
+                  <li>
+                    Deliver instant quotes for multiple global carbon markets.
+                  </li>
+                  <li>
+                    Provide a clear, approachable interface for manufacturing
+                    clients who may not have deep ESG expertise.
+                  </li>
+                  <li>
+                    Launch a fully functional MVP by early Q4 2023 with both
+                    branding and platform design ready for market.
+                  </li>
+                  <li>
+                    Build API integrations to enable in-platform renewable
+                    certification management, particularly through the I-REC
+                    international registry via the Evident API.
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </Container>
       </Section>
